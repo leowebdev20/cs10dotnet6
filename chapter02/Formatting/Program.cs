6edbc75cf0b1d794dcfe5633f47da560617466ed
l.seren <l.seren@r-factory.it>
2022-08-18 22:02:18 +0200

    formatting

---
+++ chapter02/Formatting/Program.cs
@@ -0,0 +1,24 @@
+﻿int numberOfApples = 12;
+decimal priceOfApples = 0.35M;
+
+Console.WriteLine(format: "{0} apples cost {1:C}", arg0: numberOfApples, arg1: priceOfApples * numberOfApples);
+
+string formatted = string.Format(format: "{0} apples cost {1:C}", numberOfApples, priceOfApples * numberOfApples);
+
+// WriteToFile(formatted);
+
+Console.WriteLine(formatted);
+
+Console.WriteLine($"{numberOfApples} apples cost {numberOfApples * priceOfApples:C}");
+
+var name = "leo";
+var surname = "rossi";
+var fullName = $"{name} {surname}";
+
+Console.WriteLine(fullName);
+
+
+string applesText = "apples";
+int applesCount = 14342;
+
+Console.WriteLine(format: "{0, -10} {1,6:N0}", arg0: applesText, arg1: applesCount);
--- chapter02/Variables/Program.cs
+++ chapter02/Variables/Program.cs
@@ -1,4 +1,6 @@
-﻿object height = 1.22;
+﻿using System.Xml;
+
+object height = 1.22;
 object name = "Leo";
 Console.WriteLine($"{name} is {height} meters tall");
 
@@ -10,7 +12,47 @@ dynamic something = "Amed";
 
 // something = 12;
 
+var decim = 100.00M;
+var ulon = 100UL;
 
 // something = new[] { 3, 4, 6 };
 
 Console.WriteLine($"Length is {something.Length}");
+
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+
+XmlDocument xml3 = new();
+
+
+// var file1 = File.CreateText("something.txt");
+// StreamWriter file2 = File.CreateText("something.txt");
+
+int number = default;
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(string) = {default(string)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+
+Console.WriteLine($"default(DateTime) = {number}");
+
+// Arrays
+string[] names;
+
+names = new string[4];
+
+string[] newNames = new[] { "Leo", "Marta", "Gianni", "Paolo" };
+
+
+names[0] = "Leo";
+names[1] = "Marta";
+names[2] = "Gianni";
+names[3] = "Paolo";
+
+for (int i = 0; i < names.Length; i++)
+{
+    // Console.WriteLine(names[i]);
+    Console.Write(names[i]);
+
+}
