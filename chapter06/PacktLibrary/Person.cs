2b2774ae68600aeae48ef013c015caf7380d90c1
l.seren <l.seren@r-factory.it>
2022-09-01 19:18:06 +0200

    methods and operators

---
+++ chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person
+{
+    //fields 
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    //method
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was bon on a {DateOfBirth:dddd}");
+    }
+
+    //static method to multiply. A questo posso accedere con Person.ProcreateWith(p1, p2)
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+    }
+
+    //instance method to multiply. A questo posso accedere con mary.ProcreateWith(p2)
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+}
---
+++ chapter06/PeopleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Packt.Shared;
+
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+//call instance method
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+Person baby2 = Person.Procreate(harry, jill);
+
+WriteLine($"{harry.Name} has {harry.Children.Count}");
+WriteLine($"{harry.Name} first child is named {harry.Children[0].Name}");
+WriteLine($"{jill.Name} first child is the {jill.Children[0].Name}");
+
+string s1 = "Hello ";
+string s2 = "World!";
+WriteLine(string.Concat(s1, s2));
+WriteLine(s1 + s2);
